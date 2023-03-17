fa8127b96ef0420d67ee4be33c0303ef4a9c76cb
Pooja Gangwar <pooja.gangwar@cyberark.com>
2023-03-17 14:28:47 +0530

    testing

--- conjur_setup.sh
+++ conjur_setup.sh
@@ -5,8 +5,6 @@ docker-compose down -v
 
 docker-compose build
 
-        #generate Master key and load master key as env variable
-
 docker-compose run --no-deps --rm conjur data-key generate > data_key
 
 export CONJUR_DATA_KEY="$(< data_key)"
@@ -22,11 +20,7 @@ echo "completed"
 echo "enter 'docker ps' to see the running containers and docker ps -a to see all the running and stoped containers"
 echo "*************"
 echo " "
-        #admin_api_key
-        #docker-compose exec conjur conjurctl account create myConjurAccount > admin_data
-        #admin_api_key=$(docker-compose exec -T conjur conjurctl role retrieve-key myConjurAccount:user:admin | tr -d '\r')
 
-        #creating the account
 echo "creating myConjurAccount for gitlab_server "
 echo "***************"
 echo "creating myConjurAccount and generating the api key and storing it at admin_api_key"
@@ -51,79 +45,18 @@ docker-compose exec client conjur authn login -u admin -p $CONJUR_AUTHN_API_KEY
 echo ****************""
 echo " "
 
-
-      #authn-jwt-gitlab configuration
+#authn-jwt-gitlab configuration
 echo "defining and loading the policies "
 echo " "
 echo "****************"
 echo " authn-jwt-gitlab configuration"
 echo "****************"
-docker-compose exec client conjur policy load root policy/jwt/authn-jwt-gitlab.yml
-# docker-compose exec client conjur policy load root policy/authn-jwt-gitlab1.yml
-       #docker-compose exec client conjur policy load root policy/cloudbees_cli/authn-jwt-cloudbees_ci.yml
 
-echo "set the variable values"
-echo "setting the jwks-uri"
+docker-compose exec -T client conjur policy load root policy/1.yml
+docker-compose exec -T client conjur policy load root policy/2.yml
+docker-compose exec -T client conjur policy load root policy/3.yml
 
-       #docker-compose exec client conjur variable values add conjur/authn-jwt/gitlab/jwks-uri 'http://gitlab_server:8080/jwtauth/conjur-jwk-set'
-
-
-       #docker-compose exec client conjur variable values add conjur/authn-jwt/gitlab/public-keys ${secretVal}
-docker-compose exec client conjur variable values add conjur/authn-jwt/gitlab_enterprise/jwks-uri 'https://gitlab.com/-/jwks/'
-       #docker-compose exec client conjur variable values add conjur/authn-jwt/gitlab_enterprise_cloudbees_ci/jwks-uri 'http://cloudbees_ci:8080/jwtauth/conjur-jwk-set'
-
-echo "setting issuer"
-docker-compose exec client conjur variable values add conjur/authn-jwt/gitlab/issuer 'https://gitlab.com'
-# docker-compose exec client conjur variable values add conjur/authn-jwt/gitlab_enterprise/issuer 'http://localhost:9000'
-        #docker-compose exec client conjur variable values add conjur/authn-jwt/gitlab_enterprise_cloudbees_ci/issuer 'http://localhost:9001'
-
-echo "setting the audience"
-docker-compose exec client conjur variable values add conjur/authn-jwt/gitlab/audience 'gitlab-server'
-# docker-compose exec client conjur variable values add conjur/authn-jwt/gitlab_enterprise/audience 'gitlab-server'
-        #docker-compose exec client conjur variable values add conjur/authn-jwt/gitlab_enterprise_cloudbees_ci/audience 'gitlab-server'
-
-echo "setting the token-app-property"
-docker-compose exec client conjur variable values add conjur/authn-jwt/gitlab/token-app-property 'gitlab_name'
-# docker-compose exec client conjur variable values add conjur/authn-jwt/gitlab_enterprise/token-app-property 'gitlab_name'
-        #docker-compose exec client conjur variable values add conjur/authn-jwt/gitlab_enterprise_cloudbees_ci/token-app-property 'gitlab_name'
-
-echo "#############"
-docker-compose exec client conjur list
-echo "completed"
-echo "##########"
-
-        #docker-compose exec client bash
-
-
-        #load policy/gitlab-projects.yml
-
-echo "loading policy gitlab-projects.yml"
-docker-compose exec client conjur policy load root policy/jwt/gitlab-projects.yml
-docker-compose exec client conjur policy load root policy/gitlab-projects1.yml
-        #docker-compose exec client conjur policy load root policy/cloudbees_cli/gitlab-projects.yml
-
-echo "set the identity path"
-docker-compose exec client conjur variable values add conjur/authn-jwt/gitlab/identity-path 'gitlab/projects'
-# docker-compose exec client conjur variable values add conjur/authn-jwt/gitlab_enterprise/identity-path 'gitlab1/projects1'
-        #docker-compose exec client conjur variable values add conjur/authn-jwt/gitlab_enterprise_cloudbees_ci/identity-path 'gitlab/projects2'
-
-        #load policy/gitlab-secrets.yml
-echo "loading secrets gitlab-secrets.yml"
-docker-compose exec client conjur policy load root policy/jwt/gitlab-secrets.yml
-docker-compose exec client conjur policy load root policy/gitlab-secrets1.yml
-        #docker-compose exec client conjur policy load root policy/cloudbees_cli/gitlab-secrets.yml
-
-docker-compose exec client conjur list
-        #echo "add secret values to the variables and store secret values into the variables listed"
-        #echo "command to pass the secret value to the variable: "
-        #echo "docker-compose exec client conjur variable values add <variable_value> <secret_value>"
-
-
-echo "populating values to the conjur secret variables"
-echo "========================================"
-docker-compose exec client conjur variable values add mbp-secret-1  "secret1"
-docker-compose exec client conjur variable values add mbp-secret-2  "secret1"
-docker-compose exec client conjur variable values add mbp-secret-3  "secret1"
-docker-compose exec client conjur variable values add mbp-secret-4  "secret1"
-
-
+docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/token-app-property 'root'
+docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/identity-path 'gitlab/root'
+docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/issuer 'http://gitlab.example.com:9080'
+docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/jwks-uri 'http://gitlab.example.com:9080/-/jwks/'
