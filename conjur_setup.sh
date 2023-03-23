87e8e293dfda925326e9c14a784fbbcc0336bf0b
Pooja Gangwar <56577577+Pooja-Gangwar@users.noreply.github.com>
2023-03-23 20:18:25 +0530

    Update conjur_setup.sh

--- conjur_setup.sh
+++ conjur_setup.sh
@@ -63,5 +63,5 @@ docker-compose exec -T client conjur policy load root policy/3.yml
 
 docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/token-app-property 'cyberark4'
 docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/identity-path 'gitlab/cyberark4'
-docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/issuer 'https://gitlab.com/'
+docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/issuer 'gitlab.com'
 docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/jwks-uri 'https://gitlab.com/-/jwks/'
