5c138f1411ea8369999a4a14c7191be0161cc462
Pooja Gangwar <56577577+Pooja-Gangwar@users.noreply.github.com>
2023-03-31 11:22:08 +0530

    Update conjur_setup.sh

--- conjur_setup.sh
+++ conjur_setup.sh
@@ -1,29 +1,28 @@
 
 
-docker-compose exec -T client conjur policy load root policy/first.yml
-docker-compose exec -T client conjur policy load root policy/second.yml
-docker-compose exec -T client conjur policy load root policy/third.yml
+
 
 # docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/token-app-property 'root'
 # docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/identity-path 'gitlab/root'
 # docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/issuer 'http://gitlab.example.com:9080'
 # docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/jwks-uri 'http://gitlab.example.com:9080/-/jwks/'
-
 # docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/token-app-property 'githlab-apps/cyberark4'
 # docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/token-app-property 'PoojaGangwarTCS'
-
 # docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/token-app-property 'namespace_name'
 
 
-docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/token-app-property 'namespace_path'
 
+docker-compose exec -T client conjur policy load root policy/first.yml
+docker-compose exec -T client conjur policy load root policy/second.yml
+docker-compose exec -T client conjur policy load root policy/third.yml
+
+docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/token-app-property 'namespace_path'
 docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/token-app-property 'cyberark4'
 docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/identity-path 'gitlab-apps'
-      # docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/issuer 'gitlab.com'
 docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/issuer 'https://gitlab.com'
 docker-compose exec -T client conjur variable values add conjur/authn-jwt/gitlab/jwks-uri 'https://gitlab.com/-/jwks/'
 
-docker exec -it conjur_client conjur variable values add Dev-Team-credential1 "UserName_temp"
-docker exec -it conjur_client conjur variable values add Dev-Team-credential2 "Password_temp"
-docker exec -it conjur_client conjur variable values add Dev-Team-credential3 "UserName_temp"
-docker exec -it conjur_client conjur variable values add Dev-Team-credential4 "Password_temp"
+docker exec -it conjur_client conjur variable values add Dev-Team-credential1 "UserName_secret1"
+docker exec -it conjur_client conjur variable values add Dev-Team-credential2 "Password_secret2"
+docker exec -it conjur_client conjur variable values add Dev-Team-credential3 "UserName_secret3"
+docker exec -it conjur_client conjur variable values add Dev-Team-credential4 "Password_secret4"
